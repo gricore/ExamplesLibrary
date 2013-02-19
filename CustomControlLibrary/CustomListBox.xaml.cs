e9ec9d30aa640bf9803aa0b3b91b1587e82a8c5d
Aleksanyan Grigor <aleksanyan91@gmail.com>
2013-02-19 23:29:42 +0400

    Publishing

---
+++ CustomControlLibrary/CustomListBox.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControlLibrary
+{
+    /// <summary>
+    /// Interaction logic for CustomListBox.xaml
+    /// </summary>
+    public partial class CustomListBox : UserControl
+    {       
+        public CustomListBox()
+        {
+            InitializeComponent();
+        }
+
+        // DependencyProperty 'ItemsSource'
+        public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(IEnumerable<string>),
+            typeof(CustomListBox), new PropertyMetadata(default(IEnumerable<string>)));
+        // Property 'ItemsSource'
+        public IEnumerable<string> ItemsSource
+        {
+            get { return (IEnumerable<string>) GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        // DependencyProperty 'SelectedIndex'
+        public static readonly DependencyProperty SelectedIndexProperty =
+            DependencyProperty.Register("SelectedIndex", typeof(int), typeof(CustomListBox), new PropertyMetadata(default(int)));
+        // Property 'SelectedIndex'
+        public int SelectedIndex
+        {
+            get { return (int) GetValue(SelectedIndexProperty); }
+            set { SetValue(SelectedIndexProperty, value); }
+        }
+    }
+}
